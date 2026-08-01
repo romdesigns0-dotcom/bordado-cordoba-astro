8c14fc367999e65746484e7b7e41780d1982d4c9
Admin Piatz <admin@piatz.com>
2026-08-01 16:58:32 -0600

    Add Microsoft Clarity tracking

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,6 +7,7 @@ export const SITE_CONFIG = {
   // Analytics & Tracking
   gaMeasurementId: "G-35TRHYNFFL",
   gtmId: "GTM-MCBKSP7R", // Configurado
+  clarityProjectId: "xvsjqs04kc",
 
   // Firebase
   firebaseProjectId: "bordados-cordoba",
